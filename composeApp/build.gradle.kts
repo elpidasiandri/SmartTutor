d496e069beeac1366cb26c5ee58ac08f892539b2
elpidasiandri <54350177+elpidasiandri@users.noreply.github.com>
2025-10-26 10:51:49 +0200

    useless import

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
-import java.util.Properties
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
